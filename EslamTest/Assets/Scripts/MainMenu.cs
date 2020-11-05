5a2da17eba4f59f87939190061cb10cf394a69e4
elghaied <elghaied.eslam@gmail.com>
2020-11-05 20:49:27 +0100

    start from zero

---
+++ EslamTest/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.Hosting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+	public void PlayTheGame()
+	{
+		SceneManager.LoadScene(sceneManager.GetActiveScene().buildIndex + 1);
+
+	}
+
+	public void QuitTheGame()
+	{
+	
+	}
+}
